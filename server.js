1110c9ba9ab82044a1fd2799ac8e732e75a12cf2
infocinc <nicolas.dutil@infocinc.com>
2014-09-28 15:57:08 -0400

    add directory

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(express.compress());
 
 app.use(express.static(__dirname + '/public', { maxAge: halfHour }));
 
+app.get('/fr', function(req,res) {
+    res.sendfile('index.html');
+});
 app.get('/', function(req, res){
 	res.setHeader('Cache-Control','public, max-age=' + (halfHour / 1000));	
     res.sendfile('index.html');
